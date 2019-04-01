a4d6f02dbfd154a238867b9c924598baec1e074f
pixelcake <contato@pixelcake.com.br>
2019-04-01 00:19:42 -0300

    user padrao para test e novasa rotas

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 users = []
 access_levels = ['visitante', 'usuário', 'administrativo', 'técnico', 'super-usuário']
+users = [User('Marco Luiz Gonzaga', 'marco', 'Dev', 'técnico', '100')]
+
+def find_user(nickname):
+   return [user for user in users if user.nickname == nickname][0]
 
 @app.route('/')
 def root():
@@ -32,6 +36,17 @@ def index():
 @app.route('/users/new')
 def new():
     return render_template('new.html', access_levels=access_levels)
+
+@app.route('/users/<nickname>', methods=["GET", "PATCH", "DELETE"])
+def show(nickname):
+   found_user = find_user(nickname)
+ 
+   return render_template('show.html', user = found_user)
+
+@app.route('/users/<nickname>/edit')
+def edit(nickname):
+   found_user = find_user(nickname)
+   return render_template('edit.html', user=found_user)
 
 
 if __name__ == '__main__':
